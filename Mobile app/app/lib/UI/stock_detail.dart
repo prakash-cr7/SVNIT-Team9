35c78c2cdb162cece97c4e283b33666e5bc9c4ad
prakash-cr7 <prakashkumarraj1@gmail.com>
2021-08-31 20:20:55 +0530

    stocks screen ui

--- Mobile app/app/lib/UI/stock_detail.dart
+++ Mobile app/app/lib/UI/stock_detail.dart
@@ -1,7 +1,9 @@
 import 'dart:math';
 
+import 'package:app/UI/constans.dart';
 import 'package:candlesticks/candlesticks.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class StockDetail extends StatefulWidget {
   @override
@@ -10,7 +12,8 @@ class StockDetail extends StatefulWidget {
 
 class _StockDetailState extends State<StockDetail> {
   List<Candle> candles = [];
-  
+  String _currentChart = 'L';
+  String _selectedChip = '1D';
 
   void addCandles() {
     for (int i = 0; i < 100; i++) {
@@ -54,11 +57,57 @@ class _StockDetailState extends State<StockDetail> {
               SizedBox(
                 height: 10,
               ),
-              Text('\$251',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w800,
-                      fontSize: 26)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('\$251',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w800,
+                          fontSize: 26)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      OutlinedButton(
+                          style: ButtonStyle(
+                            backgroundColor: _currentChart == 'C'
+                                ? MaterialStateProperty.all<Color>(
+                                    UiColors.primaryColor)
+                                : null,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _currentChart = 'C';
+                            });
+                          },
+                          child: Text(
+                            'C',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          )),
+                      OutlinedButton(
+                          style: ButtonStyle(
+                            backgroundColor: _currentChart == 'L'
+                                ? MaterialStateProperty.all<Color>(
+                                    UiColors.primaryColor)
+                                : null,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _currentChart = 'L';
+                            });
+                          },
+                          child: Text(
+                            'L',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ))
+                    ],
+                  )
+                ],
+              ),
               SizedBox(
                 height: 10,
               ),
@@ -72,10 +121,104 @@ class _StockDetailState extends State<StockDetail> {
               ),
               SizedBox(
                 height: MediaQuery.of(context).size.height * 0.4,
-                child: Candlesticks(
-                    candles: candles,
-                    onIntervalChange: (data) async {},
-                    interval: "1d"),
+                child: _currentChart == 'C'
+                    ? Candlesticks(
+                        candles: candles,
+                        onIntervalChange: (data) async {},
+                        interval: "1d")
+                    : SfCartesianChart(
+                        primaryXAxis: CategoryAxis(),
+                        series: <LineSeries<StockData, String>>[
+                            LineSeries<StockData, String>(
+                                // Bind data source
+                                dataSource: <StockData>[
+                                  StockData('1', 35.0),
+                                  StockData('2', 28),
+                                  StockData('3', 34),
+                                  StockData('4', 32),
+                                  StockData('5', 40)
+                                ],
+                                xValueMapper: (StockData stock, _) =>
+                                    stock.time,
+                                yValueMapper: (StockData sales, _) =>
+                                    sales.price)
+                          ]),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _selectedChip = '1D';
+                      });
+                    },
+                    child: Chip(
+                      label: Text('1D'),
+                      backgroundColor:
+                          _selectedChip == '1D' ? UiColors.primaryColor : null,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _selectedChip = '1W';
+                      });
+                    },
+                    child: Chip(
+                      label: Text('1W'),
+                      backgroundColor:
+                          _selectedChip == '1W' ? UiColors.primaryColor : null,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _selectedChip = '1M';
+                      });
+                    },
+                    child: Chip(
+                      label: Text('1M'),
+                      backgroundColor:
+                          _selectedChip == '1M' ? UiColors.primaryColor : null,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _selectedChip = 'ALL';
+                      });
+                    },
+                    child: Chip(
+                      label: Text('ALL'),
+                      backgroundColor:
+                          _selectedChip == 'ALL' ? UiColors.primaryColor : null,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                'About Apple inc',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w800),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                "Apple, Inc. engages in the design, manufacture, and marketing of mobile communication, media devices, personal computers, and portable digital music players. It operates through the following geographical segments: Americas, Europe, Greater China, Japan, and Rest of Asia Pacific. The Americas segment includes North and South America. The Europe segment consists of European countries, as well as India, the Middle East, and Africa. The Greater China segment comprises of China, Hong Kong, and Taiwan. The Rest of Asia Pacific segment includes Australia and Asian countries. The company was founded by Steven Paul Jobs, Ronald Gerald Wayne, and Stephen G. Wozniak on April 1, 1976 and is headquartered in Cupertino, CA.",
+                style: TextStyle(color: Colors.grey[200], fontSize: 14),
+              ),
+              SizedBox(
+                height: 20,
               )
             ],
           ),
@@ -83,4 +226,11 @@ class _StockDetailState extends State<StockDetail> {
       ),
     );
   }
+}
+
+class StockData {
+  final String? time;
+  final double? price;
+
+  StockData(this.time, this.price);
 }
